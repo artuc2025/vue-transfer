a4b9583c70e8c9e06b7d7ec341b6f5aa976c2dda
artuc2025 <artuc2025@gmail.com>
2025-04-22 18:36:18 +0400

    Update and rename baseFormInput.vue to baseInput.vue

--- components/baseComponents/baseInput.vue
+++ components/baseComponents/baseInput.vue
@@ -34,21 +34,7 @@ const props = defineProps({
 defineEmits(["update:modelValue"]);
 </script>
 
-<style>
-:root {
-  --color-disabled-text: rgba(91, 91, 93, 1);
-  --color-message-basic: rgba(8, 22, 61, 1);
-  --color-message-warn: rgba(220, 117, 25, 1);
-  --color-message-error: rgba(207, 73, 73, 1);
-
-  --color-symbol-border: rgba(227, 227, 237, 1);
-
-  --color-border-default: rgba(227, 227, 237, 0.8);
-  --color-bg-default: rgba(249, 249, 250, 0.7);
-  --color-text-primary: rgba(62, 73, 103, 1);
-  --color-placeholder: #999;
-}
-
+<style lang="scss" scoped>
 .base-input-wrapper {
   display: flex;
   flex-direction: column;
@@ -59,14 +45,14 @@ defineEmits(["update:modelValue"]);
   padding-left: 14px;
   font-weight: 400;
   font-size: 14px;
-  color: var(--color-text-primary);
+  color: rgba(var(--v-theme-dark-blue-8));
 }
 
 .input-message-symbol-wrapper {
   position: relative;
-  border: 1px solid var(--color-border-default);
+  border: 1px solid rgba(var(--v-theme-light-gray-2), 0.8);
   border-radius: 6px;
-  background-color: var(--color-bg-default);
+  background-color: rgba(var(--v-theme-light-gray-1), 0.7);
   backdrop-filter: blur(42.400001525878906px);
 }
 
@@ -77,22 +63,13 @@ defineEmits(["update:modelValue"]);
   font-weight: 400;
   font-size: 16px;
   padding-left: 16px;
-}
-
-.base-input:focus,
-.base-input:focus-visible {
-  border: 1px solid var(--color-border-default);
   outline: none;
   box-shadow: unset;
+  color: rgba(var(--v-theme-dark-blue-8));
 }
 
 .base-input::placeholder {
-  color: var(--color-placeholder);
-}
-
-.base-input:disabled {
-  color: var(--color-disabled-text);
-  cursor: not-allowed;
+  color: rgba(var(--v-theme-dark-blue-8), 0.3);
 }
 
 .base-input-message {
@@ -108,7 +85,7 @@ defineEmits(["update:modelValue"]);
   right: 16px;
   transform: translateY(-50%);
   font-size: 16px;
-  color: var(--color-text-primary);
+  color: rgba(var(--v-theme-dark-blue-8));
   pointer-events: none;
   user-select: none;
   &::before {
@@ -116,7 +93,7 @@ defineEmits(["update:modelValue"]);
     position: absolute;
     height: 30px;
     width: 1px;
-    background-color: var(--color-symbol-border);
+    background-color: rgba(var(--v-theme-light-gray-2));
     top: 50%;
     right: calc(100% + 16px);
     transform: translateY(-50%);
@@ -124,13 +101,13 @@ defineEmits(["update:modelValue"]);
 }
 
 .base-input-message.basic {
-  color: var(--color-message-basic);
+  color: rgba(var(--v-theme-dark-blue-2));
 }
 
 .base-input-message.warn {
-  color: var(--color-message-warn);
+  color: rgba(var(--v-theme-warning));
 }
 .base-input-message.error {
-  color: var(--color-message-error);
+  color: rgba(var(--v-theme-error));
 }
 </style>
