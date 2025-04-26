b5433e1a808da9ba72eb35ed84b59760d5a5c64a
artuc2025 <artuc2025@gmail.com>
2025-04-26 23:04:55 +0400

    feat: add layout and page structure for chat and index pages

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout-container">
+    <main class="main-content">
+      <slot />
+    </main>
+  </div>
+</template>
+<style scoped>
+.layout-container {
+  background-color: var(--ui-bg);
+  height: calc(100vh - 4rem);
+}
+.main-content {
+  height: 100%;
+  margin-top: 4rem;
+}
+</style>
---
+++ app/pages/chat.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>This is chat</h1>
+</template>
